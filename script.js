c7603cc7844c7c77c7e5e8657371dcd9b8f46663
Théophile Bellintani <theophile@captp.fr>
2024-01-17 00:45:36 +0100

    Ajout de l'affichage d'un message en fonction de la couleur

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ function changerBackground() {
   let body = document.body;
 
   body.style.backgroundColor = couleur;
-
   localStorage.setItem('couleur', couleur);
 }
 
+function afficherMessage() {
+  let message = "Très bon choix ! Cette couleur est magnifique ! ☻";
+  let encartMessage = document.getElementById('encartMessage');
+  let couleurPreferee = "#800040";
+  let couleur = document.getElementById('couleur').value;
+  if (couleur == couleurPreferee) {
+    encartMessage.innerHTML = message;
+    encartMessage.classList.replace('invisible', 'visible');
+
+    setTimeout(() => {
+      encartMessage.classList.replace('visible', 'invisible');
+    }, 3000);
+  }else {
+      encartMessage.classList.replace('visible', 'invisible');
+  }
+
+}
+
+function  ChangerCouleurEtAfficherMessage() {
+  changerBackground();
+  afficherMessage();
+}
