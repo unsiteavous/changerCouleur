0240f1eec41af0504e96ecc1d50b20913abaa8d2
Théophile Bellintani <theophile@captp.fr>
2024-01-22 09:53:37 +0100

    recorrection

--- correction/script.js
+++ correction/script.js
@@ -18,30 +18,28 @@ function changerBackground() {
   localStorage.setItem('couleur', couleur);
 }
 
-function afficherMessage() {
-  let message = "Très bon choix ! Cette couleur est magnifique ! ☻";
+function afficherMessage(couleurPreferee) {
+  let message = "<p>Très bon choix ! Cette couleur est magnifique ! ☻";
   let encartMessage = document.getElementById('encartMessage');
-  let couleurPreferee = "#800040";
+
   let couleur = document.getElementById('couleur').value;
+
   if (couleur == couleurPreferee) {
-    encartMessage.innerHTML = message;
+    encartMessage.textContent = message;
     encartMessage.classList.replace('invisible', 'visible');
 
-    setTimeout(() => {
-      encartMessage.classList.replace('visible', 'invisible');
-    }, 3000);
-  }else {
-    encartMessage.classList.replace('visible', 'invisible');
+    setTimeout(effacerMessage, 3000);
+  } else {
+    effacerMessage();
   }
-
 }
 
-function  ChangerCouleurEtAfficherMessage() {
+function ChangerCouleurEtAfficherMessage() {
   changerBackground();
-  afficherMessage();
+  afficherMessage("#800040");
 }
 
-function test() {
-  alert('Bonjour');
-  document.getElementById('encartMessage').value = "test";
+function effacerMessage() {
+    encartMessage.textContent = "";
+    encartMessage.classList.replace('visible', 'invisible');
 }
